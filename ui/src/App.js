e10329634c36090dce7a298dc6aee21931f66160
Salam Rahal-Arabi <strarabi@gmail.com>
2023-11-27 16:59:03 -0800

    finish UI main page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import ButtonWithIcon from './components/ButtonWithIcon'
-import CapacityDisplay from './components/CapacityDisplay';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <div className='buttonContainer'>
-        <ButtonWithIcon buttonText="Take Off"/>
-        <ButtonWithIcon buttonText="Land"/>
-        <ButtonWithIcon buttonText="Return to Home"/>
-      </div>
-      <div className='capacitiesContainer'>
-        <CapacityDisplay type="Battery" capacity="50"/>
-        <CapacityDisplay type="Tank" capacity="80"/>
-      </div>
+      <MainPage/>
     </div>
   );
 }
--- ui/src/components/ButtonWithIcon.js
+++ ui/src/components/ButtonWithIcon.js
@@ -2,7 +2,10 @@ import './ButtonWithIcon.css'
 
 const ButtonWithIcon = (props) => {
     return (
-        <button className='button'>{props.buttonText}</button>
+        <div className="button">
+            <img className="buttonIcon" width="48" height="48" src={props.buttonIcon} alt="Button Icon"/>
+            <p className="buttonText">{props.buttonText}</p>
+        </div>
     )
 }
 

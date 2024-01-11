a95f3657368d61c6cbb5cf18e4972dcdf568930a
Leopold-Klotz <25940620+Leopold-Klotz@users.noreply.github.com>
2024-01-10 17:03:05 -0800

    flask application connects to the drone.

--- backend/app.py
+++ backend/app.py
@@ -9,30 +9,10 @@ def hello_world():
 # Establish a connection to the drone ----------------- CHANGE THIS WHEN MULTI-OS CONNECTION SUPPORT IS ADDED
 @app.route('/connect')
 def connect():
-    from mavsdk import System
-    import asyncio
-
-    async def run():
-        print("Connecting to drone...")
-        drone = System()
-
-        # ls /dev/tty* #(mac)
-        # Replace with the address of the serial port the telemetry radio is connected to. The Baudrate is set to 57600 (default)
-        await drone.connect(system_address = "serial:///dev/tty.usbserial-D30EZV7Q:57600")
-
-        async for state in drone.core.connection_state():
-            if state.is_connected:
-                print("Drone discovered!")
-
-                # wait for user to press enter
-                input("Press Enter to exit...\n")
-
-                break
-
-    if __name__ == "__main__":
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(run())
-
+    from functions import find_serial_port, connectToDrone
+    serialPort = find_serial_port()
+    drone = connectToDrone(serialPort)
+    print("Drone connected!")
     return 'Drone connected!'
 
 if __name__ == '__main__':

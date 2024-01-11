6ceb796bd4ce865603200e6ec480b2063866ea0f
Leopold-Klotz <25940620+Leopold-Klotz@users.noreply.github.com>
2024-01-10 16:58:01 -0800

    Finds serial port and can connect to drone in generalized manner.

--- backend/functions.py
+++ backend/functions.py
@@ -1,5 +1,47 @@
 from mavsdk import System
 import asyncio
+import os
+import serial.tools.list_ports
+import platform
+
+
+def find_serial_port():
+    system = platform.system()
+    
+    if system == "Windows":
+        return find_serial_port_windows()
+    elif system == "Darwin":
+        return find_serial_port_mac()
+    elif system == "Linux":
+        return find_serial_port_linux()
+    else:
+        raise Exception(f"Unsupported operating system: {system}")
+
+def find_serial_port_windows():
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        if "USB" in port.description.upper():
+            return port.device
+    raise Exception("No drone found on serial ports.")
+
+def find_serial_port_mac():
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        if "USB" in port.description.upper():
+            return port.device
+    raise Exception("No drone found on serial ports.")
+
+def find_serial_port_linux():
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        if "USB" in port.description.upper():
+            return port.device
+
+    # If not found using pyserial, try reading from /dev
+    for port in os.listdir('/dev'):
+        if "USB" in port.upper():
+            return '/dev/' + port
+    raise Exception("No drone found on serial ports.")
 
 async def connectToDrone(serialPort):
     '''
@@ -20,3 +62,7 @@ async def connectToDrone(serialPort):
             break
 
     return drone
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(connectToDrone(find_serial_port()))

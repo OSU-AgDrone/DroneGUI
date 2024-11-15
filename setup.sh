e470db362f9c4113b184a5795860bf81c8cabeb9
jamopopper <jace_palmer@live.com>
2024-11-15 11:12:42 -0800

    install scripts!! yay!!

--- setup.sh
+++ setup.sh
@@ -1,1 +1,30 @@
-# 
+#!/bin/bash
+echo "Script started!"
+
+# Install dependencies
+echo "Installing base dependencies..."
+sudo apt update -y
+sudo apt install python3 python3-pip git -y
+#sudo apt install python3 idle3 python3-pip git python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame -y
+sudo usermod -a -G dialout $USER
+echo "Base dependencies installed!"
+
+# create virtual environment
+echo "Creating virtual environment..."
+python3 -m venv drone-gui-venv
+source drone-gui-venv/bin/activate
+echo "Virtual environment created!"
+
+# pip3 installs
+echo "Installing backend dependencies..."
+pip3 install -r backend/requirements.txt
+#pip3 install PyYAML mavproxy --user
+echo "Backend dependencies installed!"
+
+# npm installs
+echo "Installing frontend dependencies..."
+cd ui
+npm install
+cd ..
+echo "Frontend dependencies installed!"
+
--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # start the backend in the virtual python environment created in setup.sh
+echo "Starting backend..."
 python -m flask run
 
 # start the React frontend 
+echo "Starting frontend..."
+cd ui
 npm start 
+
+echo "Launching browser..."
+open http://localhost:3000

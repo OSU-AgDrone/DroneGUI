5f595a7e44202b93f48559c21ce0b12ba1bf0b03
jamopopper <jace_palmer@live.com>
2024-11-22 12:50:37 -0800

    wuh

--- startup.sh
+++ startup.sh
@@ -10,4 +10,4 @@ cd ui
 npm start
 
 echo "Launching browser..."
-chromium-browser http://localhost:5000 --kiosk --noerrdialogs --disable-infobars --no-first-run --ozone-platform=wayland --enable-features=OverlayScrollbars --start-maximized
+#chromium-browser http://localhost:5000 --kiosk --noerrdialogs --disable-infobars --no-first-run --ozone-platform=wayland --enable-features=OverlayScrollbars --start-maximized

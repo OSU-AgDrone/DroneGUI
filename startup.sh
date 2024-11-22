11f890c6415664e1d41f7bbf849f2fe288197b40
jamopopper <jace_palmer@live.com>
2024-11-22 13:06:54 -0800

    please

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ git pull
 # start the React frontend 
 echo "Starting frontend..."
 cd ui
-npm start
+lxterminal -e --command='npm start'
 
 echo "Launching browser..."
-#chromium-browser http://localhost:5000 --kiosk --noerrdialogs --disable-infobars --no-first-run --ozone-platform=wayland --enable-features=OverlayScrollbars --start-maximized
+chromium-browser http://localhost:5000 --kiosk --noerrdialogs --disable-infobars --no-first-run --ozone-platform=wayland --enable-features=OverlayScrollbars --start-maximized

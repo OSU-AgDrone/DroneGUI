066fb20101c554c0f2e121d2d1e859f1bd61b95d
jamopopper <jace_palmer@live.com>
2024-11-22 11:14:03 -0800

    bruh moment

--- startup.sh
+++ startup.sh
@@ -7,8 +7,8 @@ git pull
 # start the React frontend 
 echo "Starting frontend..."
 cd ui
-bash -- /bin/sh -c "npm start"
+bash -- /bin/bash -c "npm start"
 cd ..
 
 echo "Launching browser..."
-firefox -kiosk -private-window http://localhost:3000
+firefox -kiosk -private-window http://localhost:5000

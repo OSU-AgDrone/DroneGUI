922e619083c75e672719f93ca428f4aef39ba9b2
jamopopper <jace_palmer@live.com>
2025-01-10 09:48:02 -0800

    added popup boxes for install script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo "Script started!"
 
 # Install dependencies
-echo "Installing base dependencies..."
+zenity --info --title="AgDrone" --text="Installing base dependencies..." --auto-close
 
 sudo apt update -y # update things
 sudo apt install python3 python3-pip git firefox -y # install python in case it's not installed
@@ -13,25 +13,34 @@ sudo apt-get install nodejs npm -y
 node -v # verifies the right Node.js version is in the environment
 npm -v # verifies the right npm version is in the environment
 
-echo "Base dependencies installed!"
+zenity --info --title="AgDrone" --text="Base dependencies installed!" --auto-close
 
 # create virtual environment
-echo "Creating virtual environment..."
+zenity --info --title="AgDrone" --text="Creating virtual environment..." --auto-close
 sudo rm -rf drone-gui-venv # remove old virtual environment
 python3 -m venv drone-gui-venv # create new virtual environment
 source drone-gui-venv/bin/activate # activate virtual environment
-echo "Virtual environment created!"
+zenity --info --title="AgDrone" --text="Virtual environment created!" --auto-close
 
 # pip3 installs
-echo "Installing backend dependencies..."
+zenity --info --title="AgDrone" --text="Installing backend dependencies..." --auto-close
 pip3 install -r backend/requirements.txt # install backend dependencies using text file
 #pip3 install PyYAML mavproxy --user
-echo "Backend dependencies installed!"
+zenity --info --title="AgDrone" --text="Backend dependencies installed!" --auto-close
 
 # npm installs
-echo "Installing frontend dependencies..."
+zenity --info --title="AgDrone" --text="Installing frontend dependencies..." --auto-close
 cd ui # go to frontend directory
 npm install # install frontend dependencies
 cd .. # go back
-echo "Frontend dependencies installed!"
+zenity --info --title="AgDrone" --text="Frontend dependencies installed!" --auto-close
+
+# check drone gui is up to date
+zenity --info --title="AgDrone" --text="Checking for updates..." --auto-close
+git pull # check for updates
+sudo apt update -y # update things
+sudo apt upgrade -y # upgrade things
+zenity --info --title="AgDrone" --text="Updates checked!" --auto-close
+
+echo "Script finished!"
 

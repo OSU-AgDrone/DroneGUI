a7d0c8c86dd7b31324078e9f923535d54a01323f
Ellie Cohen <elysemc1@gmail.com>
2025-01-17 11:09:12 -0800

    fixed formatting for load routes page, hopefully will work on pi; added automatic timestamp to save routes

--- ui/src/pages/MainPage/MainPage.js
+++ ui/src/pages/MainPage/MainPage.js
@@ -2,10 +2,10 @@ import ButtonWithIcon from '../../components/MainPageButton/MainPageButton';
 import '../../App.css';
 import './MainPage.css';
 import Ringer from '../../components/Ringer';
-import { useState } from 'react';
+import { useState } from 'react';``
 import { useTranslation } from 'react-i18next';
 import BatteryGauge from 'react-battery-gauge'
-
+import getCoordsRequest from '../SavedMapsPage/SavedMapsPage';
 
 const MainPage = (props) => {
   const { t } = useTranslation();
@@ -67,7 +67,7 @@ const MainPage = (props) => {
       <div className="buttonContainer">
         <Ringer />
         <ButtonWithIcon url="/routes" buttonIcon="https://img.icons8.com/pastel-glyph/64/route--v1.png" buttonText={t('routePlanner')} />
-        <ButtonWithIcon url="/savedMaps" buttonIcon="https://img.icons8.com/64/map.png" buttonText={t('loadRoute')} />
+        <ButtonWithIcon url="/savedMaps" buttonIcon="https://img.icons8.com/64/map.png" buttonText={t('loadRoute')} onClick={getCoordsRequest()}/>
         <ButtonWithIcon link_id="button-link-settings" div_id="button-div-settings" img_id="button-img-settings" url="/settings" buttonIcon="https://img.icons8.com/?size=100&id=2969&format=png&color=000000" alt="settings--v1"  />
         <div className="missionButtons">
           <button className="regularButton button" onClick={startMissionRequest}>{t("startMission")}</button>
--- ui/src/pages/SavedMapsPage/SavedMapsPage.js
+++ ui/src/pages/SavedMapsPage/SavedMapsPage.js
@@ -3,82 +3,59 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import '../../App.css';
 
+
+export const getCoordsRequest = () => {
+    fetch('http://127.0.0.1:5000/get-coords', {
+        method: 'get',
+        mode: 'cors',
+        headers: {'Content-Type':'application/json'}
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+    }).then(data => {
+        console.log(data)
+        displayRoutes(data)
+    }).catch(error => console.error('Error', error))
+}
+
+const displayRoutes = (coordinates) => {
+    const coordinatesList = document.getElementById('coordinatesList');
+    coordinatesList.innerHTML = ''; // Clear previous list
+
+    coordinates.forEach((coord, index) => {
+        const coordinateItem = document.createElement('div');
+        coordinateItem.classList.add('coordinate-item');
+        coordinateItem.setAttribute('tabindex', '0');
+        coordinateItem.textContent = `${coord.name}`;
+        coordinateItem.onclick = () => loadCoordinate(coord); // Handle selection
+        coordinatesList.appendChild(coordinateItem);
+    });
+};
+
+const loadCoordinate = (coord) => {
+    console.log('Loaded coordinate:', coord.name);
+    // TODO add logic to actually send this route to the map/drone, save on frontend somehow
+    // TODO reflect this also in "Current Route" on homepage
+    console.log(coord);
+};
+
 const SavedMapsPage = (props) => {
+    // TODO apply translation to select a route text, select text, and all modal content 
     const { t } = useTranslation();
-
-    const getCoordsRequest = () => {
-        fetch('http://127.0.0.1:5000/get-coords', {
-            method: 'get',
-            mode: 'cors',
-            headers: {'Content-Type':'application/json'}
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error: ${response.status}`);
-            }
-            return response.json();
-        }).then(data => {
-            console.log(data)
-            displayRoutes(data)
-        }).catch(error => console.error('Error', error))
-    }
-
-    const displayRoutes = (coordinates) => {
-        const coordinatesList = document.getElementById('coordinatesList');
-        coordinatesList.innerHTML = ''; // Clear previous list
-    
-        coordinates.forEach((coord, index) => {
-            const coordinateItem = document.createElement('div');
-            coordinateItem.classList.add('coordinate-item');
-            coordinateItem.textContent = `${coord.name}`;
-            coordinateItem.onclick = () => loadCoordinate(coord); // Handle selection
-            coordinatesList.appendChild(coordinateItem);
-        });
-    
-        // show the modal
-        document.getElementById('coordinateModal').style.display = 'block';
-    };
-
-    const closeModal = () => {
-        document.getElementById('coordinateModal').style.display = 'none';
-    };
-
-    const loadCoordinate = (coord) => {
-        console.log('Loaded coordinate:', coord.name);
-        // TODO add logic to actually send this route to the map/drone, save on frontend somehow
-        // TODO reflect this also in "Current Route" on homepage
-        console.log(coord);
-        // close the modal after selection
-        closeModal();
-    };
 
     return (
         <>
+            <body onload={getCoordsRequest()}></body>
             <h1 className='title'>{t("savedMaps")}</h1>
-            <div className='pageContainer'>
-                <div className='button-container'>
-                    <button className='button findDroneButton' style={{ "marginTop": "3rem" }} onClick={getCoordsRequest}>
-                        {t("loadMap")}
-                    </button>
-                    <Link className='regularButton' to='/'>
-                        {t("back")}
-                    </Link>
-                </div>
-            </div>
-
-            {/* <!-- Modal for showing coords/routes --> */}
-            <div id="coordinateModal" class="modal">
             <div class="modal-content">
-                <h2 id="modalHeader">Select a Route</h2>
+                <h2 id="modalHeader">Select a Route</h2> 
                 <div id="coordinatesList"></div>
-                <button onClick={closeModal}>Close</button>
-            </div>
-            </div>
-            
-            <div style={{ bottom: "0", fontSize:"xx-small",marginBottom:"0rem", padding:"0" }}>
-                <a  style={{ bottom: "0", position: "absolute", marginBottom:"0rem", padding:"0"}} href="https://icons8.com/icon/12662/airplane-landing">Airplane Landing</a>
-                <a style={{ bottom: "0",  left: "4.5rem", position: "absolute", marginBottom:"0rem", padding:"0"}} href="https://icons8.com/icon/2487/airplane-take-off" target="_blank" rel="noopener noreferrer">Airplane Take Off</a>
-                <p style={{ bottom: "0", left: "9rem", position: "absolute", marginBottom:"0rem", padding:"0" }} > icon by</p>
-                <a style={{ bottom: "0", left: "11rem", position: "absolute", marginBottom:"0", padding:"0"}} href="https://icons8.com" target="_blank" rel="noopener noreferrer">Icons8</a>
+                <Link className='button' class="button routeButton" onClick={null/*TODO actually load*/}>Select</Link>
+                <Link className='button' class=' button routeButton' to='/'>
+                {t("back")}
+                </Link>
             </div>
         </>
     );

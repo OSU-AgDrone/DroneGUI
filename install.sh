c0123839feaf204e3c8c4770fa2bbdfbd20bf01d
jamopopper <jace_palmer@live.com>
2025-01-31 11:41:35 -0800

    dude i am about to give up

--- install.sh
+++ install.sh
@@ -3,12 +3,9 @@ sudo apt install git -y # install git
 
 cd /dev # move to dev folder
 sudo git clone https://github.com/OSU-AgDrone/DroneGUI.git # clone repository
-wait
 cd DroneGUI # move to repository
 sudo git checkout main # checkout main branch
-wait
 sudo ./setup.sh # run setup script
-wait
 
 sudo cp drone_boot.service /etc/systemd/system/drone_boot.service # adding startup script to init folder
 sudo systemctl enable drone_boot.service # enable service

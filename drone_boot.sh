9664e21f51842c66f8dc89fc3a7fe82cab635706
jamopopper <jace_palmer@live.com>
2025-01-31 10:42:22 -0800

    woo hoo please install the stuff properly

--- drone_boot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ../../dev/DroneGUI # move to dev/drone folder
-./setup.sh # run setup script
-wait # wait for setup to finish
-./startup.sh # run startup script
--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 sudo apt install git -y # install git 
+
+cd /dev # move to dev folder
 git clone https://github.com/OSU-AgDrone/DroneGUI.git # clone repository
 cd DroneGUI # move to repository
 git checkout main # checkout main branch
-sudo cp drone_boot.sh /etc/init.d/drone_boot.sh # adding startup script to init folder
+
+sudo cp drone_boot.service /etc/systemd/system/drone_boot.service # adding startup script to init folder
+sudo systemctl enable drone_boot.service # enable service
+sudo systemctl start drone_boot.service # start service

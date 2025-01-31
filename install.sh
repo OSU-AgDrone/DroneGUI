b344830ffba30a8ec431382bd0d077ac8ce46462
jamopopper <jace_palmer@live.com>
2025-01-31 09:17:25 -0800

    i need this script to work

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo apt install git -y # install git
 git clone https://github.com/OSU-AgDrone/DroneGUI.git # clone repository
 cd DroneGUI # move to repository
 git checkout main # checkout main branch
-cp drone_boot.sh /etc/init.d/drone_boot.sh # adding startup script to init folder
+sudo cp drone_boot.sh /etc/init.d/drone_boot.sh # adding startup script to init folder

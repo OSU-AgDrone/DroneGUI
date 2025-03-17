e537ee0bd526a327415ada7425099986fd9bacbb
Jace Palmer <60857104+jamopopper@users.noreply.github.com>
2025-03-16 20:23:02 -0700

    it should run the child script now

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd /dev # move to dev folder
 sudo git clone https://github.com/OSU-AgDrone/DroneGUI.git # clone repository
 cd DroneGUI # move to repository
 sudo git checkout main # checkout main branch
-sudo ./setup.sh # run setup script
+sudo bash setup.sh # run setup script
 
 sudo cp drone_boot.service /etc/systemd/system/drone_boot.service # adding startup script to init folder
 sudo systemctl enable drone_boot.service # enable service
